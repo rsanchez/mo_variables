d97c2c04c3d6fbf07c1cf9b436f6b8eca7809941
Rob Sanchez <rsanchez@barrettnewton.com>
2012-09-26 08:36:03 -0500

    made it so only member_group_conditionals are run more than once (in embeds)

--- system/expressionengine/third_party/mo_variables/ext.mo_variables.php
+++ system/expressionengine/third_party/mo_variables/ext.mo_variables.php
@@ -25,6 +25,12 @@ class Mo_variables_ext
 		'member_variables',
 		'member_group_conditionals',
 	);
+
+	//only these methods will be run more than once,
+	//eg. in embedded templates
+	protected $run_multiple = array(
+		'member_group_conditionals',
+	);
 	
 	protected $template_data = '';
 	
@@ -179,6 +185,14 @@ class Mo_variables_ext
 		{
 			if (method_exists($this, $method))
 			{
+				if ($this->EE->session->cache(__CLASS__, $method) && ! in_array($method, $this->run_multiple))
+				{
+					//don't run this method on subsequent runs
+					continue;
+				}
+				
+				$this->EE->session->set_cache(__CLASS__, $method, TRUE);
+				
 				$this->{$method}();
 			}
 		}
