07d184a0a64f9ff898eb06eb9fd28c76e0212465
Chris LeBlanc <chris@eedfw.us>
2012-12-10 05:01:42 -0600

    Fixing my stupid tabs. I blame git.

--- system/expressionengine/third_party/mo_variables/ext.mo_variables.php
+++ system/expressionengine/third_party/mo_variables/ext.mo_variables.php
@@ -381,13 +381,13 @@ class Mo_variables_ext
 	{
 		$secure = !empty($_SERVER['HTTPS']);
 		
-        $this->set_global_var('secure', $secure);
-         
-        $this->set_global_var('not_secure', ! $secure);
-         
-        $this->set_global_var('secure_site_url', preg_replace("/^http[s]?:/", "https:", $this->EE->config->item('site_url')));
-        
-        $this->set_global_var('insecure_site_url', preg_replace("/^http[s]?:/", "http:", $this->EE->config->item('site_url')));
+		$this->set_global_var('secure', $secure);
+
+		$this->set_global_var('not_secure', ! $secure);
+
+		$this->set_global_var('secure_site_url', preg_replace("/^http[s]?:/", "https:", $this->EE->config->item('site_url')));
+
+		$this->set_global_var('insecure_site_url', preg_replace("/^http[s]?:/", "http:", $this->EE->config->item('site_url')));
 	}
 	
 	/**
